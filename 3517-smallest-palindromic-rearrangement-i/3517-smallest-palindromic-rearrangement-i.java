82f0727e033cd8347fbd6e473274492d14e163d5
KAMESH <189806260+kamesh330@users.noreply.github.com>
2026-07-28 17:04:39 +0530

    Time: 350 ms (5.22%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -2,40 +2,24 @@ class Solution {
     public String smallestPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
         Map<Character, Integer> map = new TreeMap<>();
-        for (char i : s.toCharArray())
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        // Character arr[] = new Character[s.length()];
+        for (char i : s.toCharArray())     map.put(i, map.getOrDefault(i, 0) + 1);
         char[] arr = new char[s.length()];
         int idx = 0, n = s.length() - 1, count = 0, oddcount = 0;
         char oddvalue = 'a';
         Map<Character, Integer> list = new TreeMap<>();
-        //    List<Map.Entry<Character, Integer>> list = new ArrayList<>(map1.entrySet());
-
-        for (Map.Entry<Character, Integer> pair : map.entrySet()) {
+       for (Map.Entry<Character, Integer> pair : map.entrySet()) {
             count = 0;
-            if (pair.getValue() % 2 == 1) {
-                while (count < (pair.getValue() / 2)) {
-                arr[idx] = pair.getKey();
-                arr[n - idx] = pair.getKey();
-                idx++;
-                count++;
-                }
-                arr[n/2]=pair.getKey();
-                continue;
-            }else{
             while (count < (pair.getValue() / 2)) {
                 arr[idx] = pair.getKey();
                 arr[n - idx] = pair.getKey();
                 idx++;
                 count++;
             }
+            if (pair.getValue() % 2 == 1) {
+                arr[n/2]=pair.getKey();
             }
-
-        }
-       
+        }       
         return String.valueOf(arr);
 
-        // return " ";
-        // 
     }
 }
