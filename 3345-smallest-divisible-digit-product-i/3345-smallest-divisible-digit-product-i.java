3ee1c8b1556772b2dc9377e198762970a45ae836
KAMESH <189806260+kamesh330@users.noreply.github.com>
2026-08-06 10:09:07 +0530

    Time: 1 ms (100%), Space: 42.8 MB (14.66%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int ProductCalculation(int num){
-        int product=1;
-        while(num>0){
-            product*=num%10;
-            num=num/10;
+    public int ProductCalculation(int num) {
+        int product = 1;
+        while (num > 0) {
+            product *= num % 10;
+            num = num / 10;
         }
-        // System.out.println(product);
         return product;
     }
+
     public int smallestNumber(int n, int t) {
-        int temp=n;
-        boolean flag=true;
-        int product=0;
-        while(!(ProductCalculation(temp)%t==0)){ temp++;}
-         product=ProductCalculation(temp);
-        // System.out.println(temp+" "+product);
+        int temp = n;
+        boolean flag = true;
+        int product = 0;
+        while (!(ProductCalculation(temp) % t == 0)) {
+            temp++;
+        }
+        product = ProductCalculation(temp);
         return temp;
     }
 }
