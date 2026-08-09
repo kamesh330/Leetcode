f52a953be9e3de18ecfa994b0e5ad3ae0c07f2e7
KAMESH <189806260+kamesh330@users.noreply.github.com>
2026-08-09 15:08:55 +0530

    Time: 1 ms (97.81%), Space: 42.9 MB (8.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        if(x<=3) return 1;
+        int mid=0,left=1,right=x;
+        int ans=1;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if((x/mid)<mid){
+                right=mid;
+            }
+            else{
+                ans=mid;
+                left=mid+1;
+            }
+        }
+        // System.out.println(left+" "+right);
+        return ans;
+    }
+}
