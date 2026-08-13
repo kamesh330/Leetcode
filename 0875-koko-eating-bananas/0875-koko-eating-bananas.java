737e2b38c27b34416f66884c6870e2894ec77473
KAMESH <189806260+kamesh330@users.noreply.github.com>
2026-08-13 20:50:23 +0530

    Time: 12 ms (50.51%), Space: 48.6 MB (6.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,35 +1,26 @@
 class Solution {
-    public boolean koko(int nums[],int mid,int h){
-        long sum=0,d=0;
-        for(int i:nums){
-            sum+=(i/mid);
-            if(i%mid!=0) sum++;
+    public boolean koko(int nums[], int mid, int h) {
+        long sum = 0;
+        for (int i : nums) {
+            sum += (i / mid);
+            if (i % mid != 0)
+                sum++;
         }
-        System.out.println(sum);
-        return sum>h;
+        return sum <= h;
     }
+
     public int minEatingSpeed(int[] piles, int h) {
-        int left=1,right=1000_000_000;
-        int ans=-1,mid=0;
-        while(left<=right){
-         mid=(left+right)/2;
-        System.out.println("left="+left+" right="+right +" ans"+ans);
-
-        if(koko(piles,mid,h)){
-            ans=mid;
-            // right=mid-1;
-            left=mid+1;
+        int left = 1, right = 1000000000;
+        int mid = 0, ans = right;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (koko(piles, mid, h)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        else{
-            // ans=mid;
-            // left=mid+1;
-            right=mid-1;
-
-        }
-        System.out.println("------->left="+left+" right="+right +" ans"+ans);
-
-        }
-        System.out.println("left="+left+" right="+right +" ");
-        return left;
+        return ans;
     }
 }
